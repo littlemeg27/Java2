57da2dea050beefe721e52bce3405caa6c66958c
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2015-10-03 16:24:15 -0400

    Working on getting the list frag to show

--- Java2Project1/app/src/main/java/com/example/ravenmargret/java2project1/MainActivity.java
+++ Java2Project1/app/src/main/java/com/example/ravenmargret/java2project1/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.ravenmargret.java2project1;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
-public class MainActivity extends ActionBarActivity
+public class MainActivity extends ActionBarActivity implements ItemFragment.On
 {
+    FragmentManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        manager = getFragmentManager();
+    }
+
+    private void showMasterFragment(Fragment masterFrag)
+    {
+        manager.beginTransaction().add(R.id.container, masterFrag).commit();
     }
 }
