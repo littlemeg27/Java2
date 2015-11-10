d2b7c75b46d50f5ab7843ed4489c2819ed7eafda
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2015-11-09 23:19:11 -0500

    Working on project 3 created the activity files

---
+++ Java2Project3/app/src/main/java/com/example/ravenmargret/java2project3/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.ravenmargret.java2project3;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends ActionBarActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
---
+++ Java2Project3/app/src/main/java/com/example/ravenmargret/java2project3/ListActivity.java
@@ -0,0 +1,15 @@
+package com.example.ravenmargret.java2project3;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class ListActivity extends ActionBarActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
